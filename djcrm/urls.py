9a02a6a82906426ca97d9860870e9e7c2139d344
Muhammad Anas <iam.muhammadanas0716@gmail.com>
2024-01-13 21:02:33 +0500

    Add login redirect URL and logout functionality

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import include, path
 from leads.views import LandingPageView
 
@@ -11,8 +11,9 @@ urlpatterns = [
     path('', LandingPageView.as_view(), name='landing-page'),
     path('leads/', include('leads.urls', namespace='leads')),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
